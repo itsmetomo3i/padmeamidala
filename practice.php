6ced90a548d1f4713e754e467806b1fa93995e9d
itsmetomo3i <tomm.k1030@gmail.com>
2020-07-06 07:38:47 +0000

    For Kadai at txtbk PHP/Laravel02

---
+++ practice.php
@@ -0,0 +1,17 @@
+<?php
+$a = 3;
+$b = 7;
+echo $a+$b;
+echo "\n";
+$array_month = ["January", "February", "March", "April","May", "June","July","August","September", "October","November","December"];
+echo $array_month[7];
+echo "\n";
+$hello = "Hello ";
+$name = "Tomomi";
+$world = "'s World !!";
+echo $hello, $name, $world;
+echo "\n";
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+echo "\n";
