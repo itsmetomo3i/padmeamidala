c6fdd956087205233e1889c48173272ab35fc2a6
itsmetomo3i <tomm.k1030@gmail.com>
2020-08-01 09:10:10 +0000

    update

--- Function.php
+++ Function.php
@@ -20,22 +20,25 @@ $arr = array (1,3,5,7,9);
 function multiply ($arr){
     $result = 1;
     foreach($arr as $num){
-        $result = $result * $num;
+        $result= $result *= $num;
     }
+    return $result;
  }
  //From here
-echo $array(1,3,5,7,9);
+echo multiply($arr);
 echo "\n";
 
 //4.【応用】　次のプログラムは、配列の中で1番大きい値を返す max_array という関数を実装しようとしています。途中の部分を完成させてください。
+
 function max_array($arr){
     // とりあえず配列の最初の要素を一番大きい値とする
 $max_number = $arr[0];
-foreach($arr as $a){
- if ($max_number < $a){
-   //From here
+    $result = 0;
+  foreach($arr as $a) {
+     if($max_number < $a){
+      $max_number = $a;
  }
 }
 return $max_number;
 }
-echo max_array(1,3,5,7,9);
+echo max_array($arr);
