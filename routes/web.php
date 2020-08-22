64c843be279be71ae1ceee4c916744b8e8133035
itsmetomo3i <tomm.k1030@gmail.com>
2020-08-22 05:47:01 +0000

    Kadai-10Controller-view

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,10 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     
 //課題3「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。以下21列が解答です＊＊確認後消します
-     Route::get('/XXX', 'AAAController@bbb');
+     Route::get('xxx/XXX', 'AAAController@bbb');
      
 //【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。
-     Route::get('profile/create', 'Admin\ProfileController@edit');
-    
+     Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::get('profile/edit', 'Admin\ProfileController@edit');
     
 });
