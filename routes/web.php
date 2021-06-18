6a962490686fdb5ebe56f3fe67590fcda2f02076
itsmetomo3i <tomm.k1030@gmail.com>
2021-06-18 00:37:14 +0000

    Laravel 12 ユーザ 認証を実装する

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     
 //課題3「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。以下21列が解答です＊＊確認後消します
      
 //【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。
-     Route::get('profile/create', 'Admin\ProfileController@add');
-     Route::get('profile/edit', 'Admin\ProfileController@edit');
+     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
     
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
