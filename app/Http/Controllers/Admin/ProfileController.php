ddb76a726a847a0733c06e7268120d4b8bceacbb
itsmetomo3i <tomm.k1030@gmail.com>
2021-06-29 20:25:14 +0000

    Laravel 16課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -22,19 +22,48 @@ class ProfileController extends Controller
       $form = $request->all();
       
       unset($form['_token']);
+      //課題１ from Laravel 16
       $profile->fill($form);
       $profile->save();
       
-      return view('admin.profile.create');
+      return redirect('admin.profile.create');
     }
+public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('id', $cond_title)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
 
-    public function edit()
+public function edit(Request $request)
+  {
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+public function update(Request $request)
     {
-         return view('admin.profile.create');
-    }
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      
+      
+      unset($profile_form['_token']);
 
-    public function update()
-    {
-         return view('admin.profile.create');
+      // 該当するデータを上書きして保存する
+     $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,21 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit','Admin\NewsController@edit');
+    Route::post('news/edit','Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
     
-    
-//課題3「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。以下21列が解答です＊＊確認後消します
-     
 //【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。
      Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+
      Route::post('profile/create','Admin\ProfileController@create');
+     Route::get('profile/edit', 'Admin\ProfileController@edit');  
      Route::post('profile/edit','Admin\ProfileController@update');
+     Route::get('profile','Admin\ProfileController@index');
+     Route::post('Profile/edit','Admin\ProfileController@update');
+     Route::get('Profile/delete', 'Admin\ProfileController@delete');
 });
-//ここまで
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
